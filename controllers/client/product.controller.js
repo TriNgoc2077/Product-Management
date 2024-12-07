50b1f24a9aaaa0896cf1dadf5ddf7b24eaf0d1ab
Tri Ngoc <caotringoc11@gmail.com>
2024-12-07 16:15:31 +0700

    query + load

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,19 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model');
+
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        status: "active",
+        deleted: "false"
+    });
+
+    const newProducts = products.map(item => {
+        item.priceNew = parseInt(item.price * (1 - item.discountPercentage * 0.01)); 
+        return item;
+    });
+
     res.render("client/pages/products/index.pug", {
-        titlePage: "Product"
+        titlePage: "Product",
+        products: newProducts
     });
 }
