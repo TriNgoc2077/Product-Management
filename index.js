59099607ea70f5bfe5923a577985c44c45bde6ba
Tri Ngoc <caotringoc11@gmail.com>
2024-12-08 20:03:18 +0700

    modified admin dashboard

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const database = require('./config/database');
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
 
-
 database.connect();
+
+const systemConfig = require("./config/system");
 
 const app = express();
 const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// App Locals Variables
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+//app.localv."name var" => variable prefixAdmin exists in all project files
 
 app.use(express.static("public"));
 
