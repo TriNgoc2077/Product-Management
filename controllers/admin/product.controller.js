5f919e47da2f6f4de78c1be429d654a68dab06a6
Tri Ngoc <caotringoc11@gmail.com>
2024-12-09 17:44:46 +0700

    pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -6,6 +6,7 @@ const searchHelpers = require("../../helpers/search");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     //req.query: (require) query attributes in url, ex:localhost....?status="active"
+    
     //filter
     const filterStatus = filterStatusHelpers(req.query);
 
@@ -22,12 +23,33 @@ module.exports.index = async (req, res) => {
         find.title = objectSearch.regex;
     } 
 
-    const products = await Product.find(find);
+    //Pagination
+    let objectPagination = {
+        limitItem: 4,
+        currentPage: 1
+    };
+    if(req.query.page) {
+        objectPagination.currentPage = 
+            isNaN(parseInt(req.query.page)) ? 1 : parseInt(req.query.page);
+    }
+    const countProducts = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
+    objectPagination.totalPage = totalPage;
+    
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+    //End pagination
+
+    const products = 
+        await Product
+            .find(find)
+            .limit(objectPagination.limitItem)
+            .skip(objectPagination.skip);
 
     res.render("admin/pages/products/index.pug", {
         titlePage: "Product List",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 }
