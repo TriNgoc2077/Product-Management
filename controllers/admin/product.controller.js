6d642e79835188fde43808026cf1ab46e43c120e
Tri Ngoc <caotringoc11@gmail.com>
2024-12-09 12:27:58 +0700

    take data from database

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,14 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+    console.log(products);
+
     res.render("admin/pages/products/index.pug", {
-        titlePage: "Product List"
+        titlePage: "Product List",
+        products: products
     });
 }
