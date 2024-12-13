6d4f1348218b3ad1dfa7beed5dd61c83e60c3d5f
TriNgoc <caotringoc11@gmail.com>
2024-12-14 02:56:11 +0700

    edit feature edit patch/edit.pug

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -171,3 +171,49 @@ module.exports.createPost = async (req, res) => {
     res.redirect(`${systemConfig.prefixAdmin}/products`);
 }
 
+//[GET] //admin/products/edit/:id
+module.exports.edit = async (req, res) => {
+    // console.log(req.params.id);
+    
+    try {
+        const find = {
+            deleted: false,
+            _id: req.params.id
+        };
+
+        const product = await Product.findOne(find);
+        // console.log(product);
+
+        res.render("admin/pages/products/edit.pug", {
+            pageTitle: "Edit product",
+            product: product
+        });
+    } catch(error) {
+        res.redirect(`${systemConfig.prefixAdmin}/products`);
+    }
+}
+
+//[PATCH] //admin/products/edit/:id
+module.exports.editPatch = async (req, res) => {
+    // console.log(req.body);
+    req.body.price = parseInt(req.body.price);
+    req.body.discountPercentage = parseInt(req.body.discountPercentage);
+    req.body.stock = parseInt(req.body.stock);
+    req.body.position = parseInt(req.body.position);
+    if (req.file) {
+        req.body.thumbnail = `/uploads/${req.file.filename}`;
+    }
+
+    try {
+        await Product.updateOne(
+            { _id: req.params.id }, 
+            req.body
+        );
+        req.flash("success", "Update Successfully!");
+    } catch(error) {
+        console.log(error);
+        req.flash("error", "Update Failed !");
+    }
+
+    res.redirect("back");
+}
--- validates/admin/product.validate.js
+++ validates/admin/product.validate.js
@@ -1,6 +1,6 @@
 module.exports.createPost = (req, res, next) => {
     if (!req.body.title) {
-        req.flash("error", "Please enter title !");
+        req.flash("error", "Please enter the title !");
         res.redirect("back");
         return;
     }
