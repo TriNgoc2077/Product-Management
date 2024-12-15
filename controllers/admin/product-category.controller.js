7f91090658ab38659127da8e698b781416b12445
TriNgoc <caotringoc11@gmail.com>
2024-12-16 00:47:04 +0700

    hierarchy category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -16,13 +16,43 @@ module.exports.index = async (req, res) => {
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false
+    };
+
+    function createTree(arr, parentId = "") {
+        const tree = [];
+        arr.forEach((item) => {
+            if (item.parent_id == parentId) {
+                const newItem = item;
+                // console.log(item._id.toString());
+                const children = createTree(arr, item._id);
+                if (children.length > 0) {
+                    newItem.children = children;
+                }
+                console.log(newItem);
+                tree.push(newItem);
+            }
+            
+        });
+        return tree;
+    }
+    
+    const records = await ProductCategory.find(find);
+    const newRecords = createTree(records);
+
+    // console.log(newRecords);
+
+
+
     res.render("admin/pages/product-category/create", {
         titlePage: "Create product Category",
+        records: records
     });
 }
 //[POST] //admin/product-categody/create
 module.exports.createPost = async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     if (req.body.position == ""){
         const count = await ProductCategory.countDocuments();
         req.body.position = count + 1;
