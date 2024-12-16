b0b35a5deebbaf867b2242f53364190deb2b4917
Tri Ngoc <caotringoc11@gmail.com>
2024-12-16 18:01:00 +0700

    edit role

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -34,3 +34,38 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
 }
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+        let find = {
+            _id: id,
+            deleted: false
+        };
+        const record = await Role.findOne(find);
+
+        res.render("admin/pages/roles/edit.pug", {
+            titlePage: "Edit Permission",
+            data: record
+        });
+    } catch {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        await Role.updateOne(
+            { _id: req.params.id }, 
+            req.body
+        );
+        req.flash("success", "Update Successfully!");
+    } catch(error) {
+        console.log(error);
+        req.flash("error", "Update Failed !");
+    }
+
+    res.redirect("back");
+}
