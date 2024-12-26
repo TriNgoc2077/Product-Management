0e45159831b45d9efab594c72571889bc42b0084
TriNgoc <caotringoc11@gmail.com>
2024-12-26 16:27:21 +0700

    private route

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,11 +1,23 @@
 const Account = require("../../models/account.model");
 const md5 = require("md5");
 const systemConfig = require("../../config/system");
+
+
 //[GET] /admin/auth/login
-module.exports.login = (req, res) => {
-    res.render("admin/pages/auth/login.pug", {
-        pageTitle: "Login"
-    });
+module.exports.login = async (req, res) => {
+    // console.log(req.cookies.token);
+    if (req.cookies.token) {
+        const user = await Account.findOne({ token: req.cookies.token });
+        if (!user) {
+            res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+        }
+        res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+    } else {
+        res.render("admin/pages/auth/login.pug", {
+            pageTitle: "Login"
+        });
+    }
+
 }
 
 //[POST] /admin/auth/login
