82f91252ffd2044d41261092798e8d627935bdd1
TriNgoc <caotringoc11@gmail.com>
2024-12-28 00:02:15 +0700

    listener change input quantity and update price

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-console.log("okok");
+console.log("ok123");
+// update price 
+const quantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#currentPrice");
+const newPrice = document.querySelector("#newPrice");
+quantity.addEventListener("change", (e) => {
+    // console.log(parseInt(newPrice.textContent.slice(1)));
+    currentPrice.textContent = (quantity.value * parseInt(newPrice.textContent.slice(1))).toFixed(1);
+});
