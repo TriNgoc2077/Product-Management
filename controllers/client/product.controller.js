f96f17b54c39b1678f5a70c29b8924d9e1226e7c
TriNgoc <caotringoc11@gmail.com>
2024-12-27 19:48:00 +0700

    display product category client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require('../../models/product.model');
 const productHelper = require("../../helpers/product");
-
+const ProductCategory = require("../../models/product-category.model");
+const { all } = require('../../routes/client/product.route');
+const productCategoryHelper = require("../../helpers/productCategory");
 // [GET] /products
 
 module.exports.index = async (req, res) => {
@@ -13,7 +15,7 @@ module.exports.index = async (req, res) => {
     const newProducts = productHelper.newPrice(products);
     
     res.render("client/pages/products/index.pug", {
-        titlePage: "Product",
+        titlePage: "Our products",
         products: newProducts,
     });
 }
@@ -39,3 +41,33 @@ module.exports.detail = async (req, res) => {
     }
 }
 
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+    try {
+        const category = await ProductCategory.findOne({
+            slug: req.params.slugCategory,
+            deleted: false
+        });
+        
+
+        const listSubCategory = await productCategoryHelper.getSubCategory(category.id);
+
+        const listSubCategoryId = listSubCategory.map(item => {
+            return item.id;
+        });
+
+        const products = await Product.find({
+            product_category_id: { $in: [category.id, ...listSubCategoryId] },
+            deleted: false
+        }).sort({ position: "desc" });
+
+        // console.log(products);
+        const newProducts = productHelper.newPrice(products);
+        res.render("client/pages/products/index.pug", {
+            titlePage: category.title,
+            products: newProducts,
+        });
+    } catch(error) {
+        res.redirect(`/products`);
+    }
+}
