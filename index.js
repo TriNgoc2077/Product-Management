8bfc2288b8fb93825f7196e41311678e22a771ca
Tri Ngoc <caotringoc11@gmail.com>
2024-12-31 13:56:11 +0700

    dashboard + update interface admin + 404 page

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.use(express.static(`${__dirname}/public`));
 routeAdmin(app);
 route(app);
 
+app.get("*", (req, res) => {
+    res.render("client/pages/errors/404", {
+        titlePages: "404 Not Found"
+    })
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
